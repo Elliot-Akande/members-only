8185365d9253c649a4a6e1f8c6840cf92b587a90
Elliot Akande <elbo.lynch@gmail.com>
2023-12-21 11:33:00 +0000

    Update max title length

--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, maxLength: 50 },
   message: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   timestamp: { type: Date, required: true },
