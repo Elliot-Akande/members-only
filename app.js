93772e1cf997806f01986175ab70904043cc92b8
Elliot Akande <elbo.lynch@gmail.com>
2023-12-22 12:02:25 +0000

    Add bcrypt to authentication middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("./models/user.js");
 
 const indexRouter = require("./routes/index");
 
@@ -26,7 +28,8 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
